8c323cef1e13afd20fecae40c5ac4472db7e5548
Kelvin Samuel Klutse <88723961+Vankelv@users.noreply.github.com>
2024-05-11 15:48:55 +0000

    Delete cloudinary.js

--- server/cloudinary.js
+++
@@ -1,7 +0,0 @@
-const cloudinary = require('cloudinary').v2;
-cloudinary.config({ 
-  cloud_name: 'dixhhfgil', 
-  api_key: '837931239471236', 
-  api_secret: 'NdO_gQFiMJVE8Zwp3CKLsNu0auc' 
-});
-module.exports = { cloudinary };
